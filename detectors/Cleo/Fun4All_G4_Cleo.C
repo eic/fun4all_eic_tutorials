6d40605a379d3b4ffbc475fdf6c5c5ca033bfd14
Chris Pinkenburg <pinkenburg@bnl.gov>
2020-08-03 12:12:44 -0400

    add cleo magnet template

---
+++ detectors/Cleo/Fun4All_G4_Cleo.C
@@ -0,0 +1,265 @@
+#ifndef MACRO_FUN4ALLG4CLEO_C
+#define MACRO_FUN4ALLG4CLEO_C
+
+#include "GlobalVariables.C"
+
+#include "DisplayOn.C"
+#include "G4Setup_Cleo.C"
+#include "G4_DSTReader_Cleo.C"
+#include "G4_Input.C"
+
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+
+#include <phool/PHRandomSeed.h>
+#include <phool/recoConsts.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libg4testbench.so)
+
+int Fun4All_G4_Cleo(
+    const int nEvents = 1,
+    const string &inputFile = "/sphenix/data/data02/review_2017-08-02/single_particle/spacal2d/fieldmap/G4Hits_sPHENIX_e-_eta0_8GeV-0002.root",
+    const string &outputFile = "G4Cleo.root",
+    const string &embed_input_file = "/sphenix/data/data02/review_2017-08-02/sHijing/fm_0-4.list",
+    const int skip = 0,
+    const string &outdir = ".")
+{
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(0);
+
+  //Opt to print all random seed used for debugging reproducibility. Comment out to reduce stdout prints.
+  PHRandomSeed::Verbosity(1);
+
+  // just if we set some flags somewhere in this macro
+  recoConsts *rc = recoConsts::instance();
+  // By default every random number generator uses
+  // PHRandomSeed() which reads /dev/urandom to get its seed
+  // if the RANDOMSEED flag is set its value is taken as seed
+  // You can either set this to a random value using PHRandomSeed()
+  // which will make all seeds identical (not sure what the point of
+  // this would be:
+  //  rc->set_IntFlag("RANDOMSEED",PHRandomSeed());
+  // or set it to a fixed value so you can debug your code
+  //  rc->set_IntFlag("RANDOMSEED", 12345);
+
+  //===============
+  // Input options
+  //===============
+  // First enable the input generators
+  // Either:
+  // read previously generated g4-hits files, in this case it opens a DST and skips
+  // the simulations step completely. The G4Setup macro is only loaded to get information
+  // about the number of layers used for the cell reco code
+  //  Input::READHITS = true;
+  INPUTREADHITS::filename = inputFile;
+
+  // Or:
+  // Use particle generator
+  // And
+  // Further choose to embed newly simulated events to a previous simulation. Not compatible with `readhits = true`
+  // In case embedding into a production output, please double check your G4Setup_sPHENIX.C and G4_*.C consistent with those in the production macro folder
+  // E.g. /sphenix/sim//sim01/production/2016-07-21/single_particle/spacal2d/
+  //  Input::EMBED = true;
+  INPUTEMBED::filename = embed_input_file;
+
+  Input::SIMPLE = true;
+  //Input::SIMPLE_VERBOSITY = 1;
+
+  //  Input::PYTHIA6 = true;
+
+  // Input::PYTHIA8 = true;
+
+  //  Input::GUN = true;
+  //Input::GUN_VERBOSITY = 1;
+
+  // Upsilon generator
+  //  Input::UPSILON = true;
+  Input::UPSILON_VERBOSITY = 0;
+
+  //  Input::HEPMC = true;
+  Input::VERBOSITY = 0;
+  INPUTHEPMC::filename = inputFile;
+
+  // Event pile up simulation with collision rate in Hz MB collisions.
+  //Input::PILEUPRATE = 100e3;
+
+  //-----------------
+  // Initialize the selected Input/Event generation
+  //-----------------
+  // This creates the input generator(s)
+  InputInit();
+
+  //--------------
+  // Set generator specific options
+  //--------------
+  // can only be set after InputInit() is called
+
+  // Simple Input generator:
+  if (Input::SIMPLE)
+  {
+    INPUTGENERATOR::SimpleEventGenerator->add_particles("pi-", 5);
+    if (Input::HEPMC || Input::EMBED)
+    {
+      INPUTGENERATOR::SimpleEventGenerator->set_reuse_existing_vertex(true);
+      INPUTGENERATOR::SimpleEventGenerator->set_existing_vertex_offset_vector(0.0, 0.0, 0.0);
+    }
+    else
+    {
+      INPUTGENERATOR::SimpleEventGenerator->set_vertex_distribution_function(PHG4SimpleEventGenerator::Uniform,
+                                                                             PHG4SimpleEventGenerator::Uniform,
+                                                                             PHG4SimpleEventGenerator::Uniform);
+      INPUTGENERATOR::SimpleEventGenerator->set_vertex_distribution_mean(0., 0., 0.);
+      INPUTGENERATOR::SimpleEventGenerator->set_vertex_distribution_width(0., 0., 5.);
+    }
+    INPUTGENERATOR::SimpleEventGenerator->set_eta_range(-1, 1);
+    INPUTGENERATOR::SimpleEventGenerator->set_phi_range(-M_PI, M_PI);
+    INPUTGENERATOR::SimpleEventGenerator->set_pt_range(0.1, 20.);
+  }
+  // Upsilons
+  if (Input::UPSILON)
+  {
+    INPUTGENERATOR::VectorMesonGenerator->add_decay_particles("e", 0);
+    INPUTGENERATOR::VectorMesonGenerator->set_rapidity_range(-1, 1);
+    INPUTGENERATOR::VectorMesonGenerator->set_pt_range(0., 10.);
+    // Y species - select only one, last one wins
+    INPUTGENERATOR::VectorMesonGenerator->set_upsilon_1s();
+  }
+  // particle gun
+  if (Input::GUN)
+  {
+    INPUTGENERATOR::Gun->AddParticle("pi-", 0, 1, 0);
+    INPUTGENERATOR::Gun->set_vtx(0, 0, 0);
+  }
+
+  //--------------
+  // Set Input Manager specific options
+  //--------------
+  // can only be set after InputInit() is called
+
+  if (Input::HEPMC)
+  {
+    INPUTMANAGER::HepMCInputManager->set_vertex_distribution_width(100e-4, 100e-4, 30, 0);  //optional collision smear in space, time
+                                                                                            //    INPUTMANAGER::HepMCInputManager->set_vertex_distribution_mean(0,0,0,0);//optional collision central position shift in space, time
+    // //optional choice of vertex distribution function in space, time
+    INPUTMANAGER::HepMCInputManager->set_vertex_distribution_function(PHHepMCGenHelper::Gaus, PHHepMCGenHelper::Gaus, PHHepMCGenHelper::Gaus, PHHepMCGenHelper::Gaus);
+    //! embedding ID for the event
+    //! positive ID is the embedded event of interest, e.g. jetty event from pythia
+    //! negative IDs are backgrounds, .e.g out of time pile up collisions
+    //! Usually, ID = 0 means the primary Au+Au collision background
+    //INPUTMANAGER::HepMCInputManager->set_embedding_id(2);
+  }
+  // register all input generators with Fun4All
+  InputRegister();
+
+  //======================
+  // Write the DST
+  //======================
+
+  //  Enable::DSTOUT = true;
+  Enable::DSTOUT_COMPRESS = false;
+  DstOut::OutputDir = outdir;
+  DstOut::OutputFile = outputFile;
+
+  //Option to convert DST to human command readable TTree for quick poke around the outputs
+  Enable::DSTREADER = true;
+
+  // turn the display on (default off)
+  Enable::DISPLAY = false;
+
+  //======================
+  // What to run
+  //======================
+
+  // whether to simulate the Be section of the beam pipe
+  Enable::PIPE = true;
+  Enable::PIPE_ABSORBER = true;
+  // EIC beam pipe extension beyond the Be-section:
+  G4PIPE::use_forward_pipes = true;
+
+  Enable::MAGNET = true;
+  Enable::MAGNET_ABSORBER = true;
+
+  // new settings using Enable namespace in GlobalVariables.C
+  Enable::BLACKHOLE = true;
+  // leave 1m gap so we can see tracks outside detector, comment out when you
+  // want to use the black hole to measure leakage
+  BlackHoleGeometry::gap = 100.; // leave 1m gap so we can see tracks outside detector
+  //Enable::BLACKHOLE_SAVEHITS = false; // turn off saving of bh hits
+  //BlackHoleGeometry::visible = true;
+
+  // establish the geometry and reconstruction setup
+  G4Init();
+
+  if (!Input::READHITS)
+  {
+    //---------------------
+    // Detector description
+    //---------------------
+
+    G4Setup();
+  }
+
+  string outputroot = outputFile;
+  string remove_this = ".root";
+  size_t pos = outputroot.find(remove_this);
+  if (pos != string::npos)
+  {
+    outputroot.erase(pos, remove_this.length());
+  }
+
+  if (Enable::DSTREADER) G4DSTreader(outputroot + "_DSTReader.root");
+
+  //--------------
+  // Set up Input Managers
+  //--------------
+
+  InputManagers();
+
+  if (Enable::DSTOUT)
+  {
+    string FullOutFile = DstOut::OutputDir + "/" + DstOut::OutputFile;
+    Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", FullOutFile);
+    if (Enable::DSTOUT_COMPRESS) DstCompress(out);
+    se->registerOutputManager(out);
+  }
+
+  //-----------------
+  // Event processing
+  //-----------------
+  if (nEvents < 0)
+  {
+    return 0;
+  }
+  // if we run the particle generator and use 0 it'll run forever
+  if (nEvents == 0 && !Input::HEPMC && !Input::READHITS)
+  {
+    cout << "using 0 for number of events is a bad idea when using particle generators" << endl;
+    cout << "it will run forever, so I just return without running anything" << endl;
+    return 0;
+  }
+
+  if (Enable::DISPLAY)
+  {
+    DisplayOn();
+    // prevent macro from finishing so can see display
+    int i;
+    cout << "***** Enter any integer to proceed" << endl;
+    cin >> i;
+  }
+
+  se->run(nEvents);
+
+  //-----
+  // Exit
+  //-----
+
+  se->End();
+  std::cout << "All done" << std::endl;
+  delete se;
+  gSystem->Exit(0);
+  return 0;
+}
+
+#endif  // MACRO_FUN4ALLG4CLEO_C
---
+++ detectors/Cleo/G4_Magnet_Cleo.C
@@ -0,0 +1,78 @@
+#ifndef MACRO_G4MAGNETJLEIC_C
+#define MACRO_G4MAGNETJLEIC_C
+
+#include "GlobalVariables.C"
+
+#include <g4detectors/PHG4CylinderSubsystem.h>
+
+#include <g4main/PHG4Reco.h>
+
+R__LOAD_LIBRARY(libg4detectors.so)
+
+namespace Enable
+{
+  bool MAGNET = false;
+  bool MAGNET_ABSORBER = false;
+  bool MAGNET_OVERLAPCHECK = false;
+  int MAGNET_VERBOSITY = 0;
+}  // namespace Enable
+
+namespace G4MAGNET
+{
+  double magnet_inner_radius = 130.;
+  double magnet_outer_radius = 143.;
+  double magnet_length = 400.;
+  double magfield_rescale = 1;
+  string magfield = string(getenv("CALIBRATIONROOT")) + string("/Field/Map/SolenoidMag3D.TABLE");
+
+}  // namespace G4MAGNET
+
+void MagnetInit()
+{
+  BlackHoleGeometry::max_radius = std::max(BlackHoleGeometry::max_radius, G4MAGNET::magnet_outer_radius);
+  BlackHoleGeometry::max_z = std::max(BlackHoleGeometry::max_z, G4MAGNET::magnet_length / 2.);
+  BlackHoleGeometry::min_z = std::min(BlackHoleGeometry::min_z, -G4MAGNET::magnet_length / 2.);
+}
+
+double Magnet(PHG4Reco* g4Reco,
+              double radius)
+{
+  bool AbsorberActive = Enable::ABSORBER || Enable::MAGNET_ABSORBER;
+  bool OverlapCheck = Enable::OVERLAPCHECK || Enable::MAGNET_OVERLAPCHECK;
+  int verbosity = std::max(Enable::VERBOSITY, Enable::MAGNET_VERBOSITY);
+
+  if (radius > G4MAGNET::magnet_inner_radius)
+  {
+    cout << "inconsistency: radius: " << radius
+         << " larger than Magnet inner radius: " << G4MAGNET::magnet_inner_radius << endl;
+    gSystem->Exit(-1);
+  }
+
+  radius = G4MAGNET::magnet_inner_radius;
+  PHG4CylinderSubsystem* cyl = new PHG4CylinderSubsystem("MAGNET", 0);
+  cyl->set_double_param("radius", G4MAGNET::magnet_inner_radius);
+  cyl->set_double_param("length", G4MAGNET::magnet_length);
+  cyl->set_double_param("thickness", G4MAGNET::magnet_outer_radius - G4MAGNET::magnet_inner_radius);
+  cyl->set_string_param("material", "Al5083");  // use 1 radiation length Al for magnet thickness
+  cyl->SuperDetector("MAGNET");
+  if (AbsorberActive) cyl->SetActive();
+  g4Reco->registerSubsystem(cyl);
+
+  radius = G4MAGNET::magnet_outer_radius;  // outside of magnet
+
+  if (verbosity > 0)
+  {
+    cout << "========================= G4_Magnet.C::Magnet() ===========================" << endl;
+    cout << " MAGNET Material Description:" << endl;
+    cout << "  inner radius = " << G4MAGNET::magnet_inner_radius << " cm" << endl;
+    cout << "  outer radius = " << G4MAGNET::magnet_outer_radius << " cm" << endl;
+    cout << "  length = " << G4MAGNET::magnet_length << " cm" << endl;
+    cout << "===========================================================================" << endl;
+  }
+
+  radius += no_overlapp;
+
+  return radius;
+}
+
+#endif  // MACRO_G4MAGNETJLEIC_C
